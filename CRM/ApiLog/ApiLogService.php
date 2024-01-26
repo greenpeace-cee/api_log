27b1c01de240bb2168cf6cf48043505c3b126462
Patrick Figel <pfigel@greenpeace.org>
2024-01-26 10:58:50 +0100

    GP-42802 Fix filters and improve exception handling

--- CRM/ApiLog/ApiLogService.php
+++ CRM/ApiLog/ApiLogService.php
@@ -41,12 +41,11 @@ class CRM_ApiLog_ApiLogService extends AutoService implements EventSubscriberInt
   }
 
   private static function shouldCheckField($filterValue, $field): bool {
-    return str_contains($filterValue, '*') || preg_match('/' . preg_quote($filterValue, '/') . '/i', $field);
+    return $filterValue == '*' || preg_match('/' . $filterValue . '/i', $field);
   }
 
   public static function handleApiEvent($event, $isSuccess): void {
     foreach (self::getConfigs() as $config) {
-
       if (self::isPassConfig($config, $event)) {
         self::logApiRequest($event, $isSuccess, $config);
       }
@@ -55,7 +54,12 @@ class CRM_ApiLog_ApiLogService extends AutoService implements EventSubscriberInt
 
   private static function logApiRequest($event, $isSuccess, $config): void {
     $apiRequest = $event->getApiRequest();
-    $apiResponse = method_exists($event, 'getResponse') ? $event->getResponse() : null;
+    if ($event instanceof \Civi\API\Event\ExceptionEvent) {
+      $apiResponse = self::formatException($event);
+    }
+    else {
+      $apiResponse = $event->getResponse();
+    }
 
     if ($apiResponse && isset($apiResponse['is_error']) && $apiResponse['is_error'] !== 0) {
       $isSuccess = false;
@@ -67,7 +71,7 @@ class CRM_ApiLog_ApiLogService extends AutoService implements EventSubscriberInt
         'api_log_config_id' => $config['id'],
         'api_entity' => $apiRequest['entity'],
         'api_action' => $apiRequest['action'],
-        'request' => json_encode($apiRequest),
+        'request' => json_encode(is_array($apiRequest) ? $apiRequest['params'] ?? [] : $apiRequest->getParams()),
         'response' => json_encode($apiResponse),
         'api_version' => $apiRequest['version'] ?? null,
         'success' => $isSuccess,
@@ -96,7 +100,7 @@ class CRM_ApiLog_ApiLogService extends AutoService implements EventSubscriberInt
       return false;
     }
 
-    if (str_contains($expression, '*')) {
+    if ($expression == '*') {
       return true;
     }
 
@@ -110,4 +114,59 @@ class CRM_ApiLog_ApiLogService extends AutoService implements EventSubscriberInt
 
     return false;
   }
+
+  /**
+   * Format an ExceptionEvent according to the implementation
+   * in \Civi\API\Kernel (API3) or CRM_Api4_Page_AJAX (API4)
+   *
+   * @param \Civi\API\Event\ExceptionEvent $event
+   *
+   * @return mixed
+   * @throws \CRM_Core_Exception
+   */
+  private static function formatException(\Civi\API\Event\ExceptionEvent $event) {
+    $apiRequest = $event->getApiRequest();
+    $e = $event->getException();
+    if ($apiRequest['version'] == 3) {
+      $kernel = $event->getApiKernel();
+      if ($e instanceof \CRM_Core_Exception) {
+        $err = $kernel->formatApiException($e, $apiRequest);
+      }
+      elseif ($e instanceof \PEAR_Exception) {
+        $err = $kernel->formatPearException($e, $apiRequest);
+      }
+      else {
+        $err = $kernel->formatException($e, $apiRequest);
+      }
+      return $kernel->formatResult($apiRequest, $err);
+    }
+    else {
+      $response = [];
+      $statusMap = [
+        \Civi\API\Exception\UnauthorizedException::class => 403,
+      ];
+      $response['status'] = $statusMap[get_class($e)] ?? 500;
+      if (method_exists($e, 'getErrorData')) {
+        $response['error_code'] = $e->getCode();
+        $response['error_message'] = $e->getMessage();
+        if (method_exists($e, 'getUserInfo')) {
+          $response['debug']['info'] = $e->getUserInfo();
+        }
+        $cause = method_exists($e, 'getCause') ? $e->getCause() : $e;
+        if ($cause instanceof \DB_Error) {
+          $response['debug']['db_error'] = \DB::errorMessage($cause->getCode());
+          $response['debug']['sql'][] = $cause->getDebugInfo();
+        }
+        // Would prefer getTrace() but that causes json_encode to bomb
+        $response['debug']['backtrace'] = $e->getTraceAsString();
+      }
+      else {
+        $response['error_code'] = 1;
+        $response['error_message'] = $e->getMessage();
+        $response['debug']['backtrace'] = $e->getTraceAsString();
+      }
+      return $response;
+    }
+  }
+
 }
